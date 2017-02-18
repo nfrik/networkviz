747f6a8f8889c2454c55d641ff303038fa8a41a0
nfrik <nfrik@iphonedreams.com>
2017-02-18 09:25:40 -0500

    New controls

--- src/main/java/core/NetworkVis.java
+++ src/main/java/core/NetworkVis.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Point3D;
 import javafx.scene.*;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -19,7 +20,6 @@ import javafx.scene.shape.Box;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Transform;
-import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -29,11 +29,19 @@ public class NetworkVis extends Application {
     final Group root = new Group();
     final XformWorld world = new XformWorld();
     final PerspectiveCamera camera = new PerspectiveCamera(true);
-    final XformCamera cameraXform = new XformCamera();
+    final Xform cameraXform = new Xform();
     private static final double CAMERA_INITIAL_DISTANCE = -1000;
+    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
     private static final double CAMERA_NEAR_CLIP = 0.1;
     private static final double CAMERA_FAR_CLIP = 100000.0;
     private static final double AXIS_LENGTH = 250.0;
+    private static final double CONTROL_MULTIPLIER = 0.1;
+    private static final double SHIFT_MULTIPLIER = 10.0;
+    private static final double MOUSE_SPEED = 1;
+    private static final double ROTATION_SPEED = 2.0;
+    private static final double TRACK_SPEED = 0.3;
+
     double mousePosX, mousePosY, mouseOldX, mouseOldY, mouseDeltaX, mouseDeltaY;
     double mouseFactorX, mouseFactorY;
     private Group axisGroup = new Group();
@@ -48,6 +56,7 @@ public class NetworkVis extends Application {
         Scene scene = new Scene(root, 800, 600, true);
         scene.setFill(Color.BLACK);
         handleMouse(scene);
+        handleKeyboard(scene);
         primaryStage.setTitle("TrafoTest");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -112,6 +121,9 @@ public class NetworkVis extends Application {
         world.getChildren().addAll(mapGraph.getEdges());
         world.getChildren().addAll(mapGraph.getVertices());
 
+        System.out.println("Total edges: "+mapGraph.getEdges().size());
+        System.out.println("Total vertices: "+mapGraph.getVertices().size());
+
         Random rn = new Random();
 
         final Timeline loop = new Timeline(new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
@@ -132,7 +144,7 @@ public class NetworkVis extends Application {
         }));
 
         loop.setCycleCount(Timeline.INDEFINITE);
-        loop.play();
+//        loop.play();
     }
 
 
@@ -160,27 +172,70 @@ public class NetworkVis extends Application {
 
 
     private void handleMouse(Scene scene) {
-        scene.setOnMousePressed((MouseEvent me) -> {
-            mousePosX = me.getSceneX();
-            mousePosY = me.getSceneY();
-            mouseOldX = me.getSceneX();
-            mouseOldY = me.getSceneY();
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseOldX = me.getSceneX();
+                mouseOldY = me.getSceneY();
+            }
         });
-        scene.setOnMouseDragged((MouseEvent me) -> {
-            mouseOldX = mousePosX;
-            mouseOldY = mousePosY;
-            mousePosX = me.getSceneX();
-            mousePosY = me.getSceneY();
-            mouseDeltaX = (mousePosX - mouseOldX);
-            mouseDeltaY = (mousePosY - mouseOldY);
-            if (me.isPrimaryButtonDown()) {
-                cameraXform.ry(mouseDeltaX * 180.0 / scene.getWidth());
-                cameraXform.rx(-mouseDeltaY * 180.0 / scene.getHeight());
-            } else if (me.isSecondaryButtonDown()) {
-                camera.setTranslateZ(camera.getTranslateZ() + mouseDeltaY);
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                mouseOldX = mousePosX;
+                mouseOldY = mousePosY;
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseDeltaX = (mousePosX - mouseOldX);
+                mouseDeltaY = (mousePosY - mouseOldY);
+
+                double modifier = 1.0;
+
+                if (me.isControlDown()) {
+                    modifier = CONTROL_MULTIPLIER;
+                }
+                if (me.isShiftDown()) {
+                    modifier = SHIFT_MULTIPLIER;
+                }
+                if (me.isPrimaryButtonDown()) {
+                    cameraXform.ry.setAngle(cameraXform.ry.getAngle() - mouseDeltaX * MOUSE_SPEED * modifier * ROTATION_SPEED);
+                    cameraXform.rx.setAngle(cameraXform.rx.getAngle() + mouseDeltaY * MOUSE_SPEED * modifier * ROTATION_SPEED);
+                } else if (me.isSecondaryButtonDown()) {
+                    double z = camera.getTranslateZ();
+                    double newZ = z + mouseDeltaX * MOUSE_SPEED * modifier;
+                    camera.setTranslateZ(newZ);
+                } else if (me.isMiddleButtonDown()) {
+                    cameraXform.t.setX(cameraXform.t.getX() + mouseDeltaX * MOUSE_SPEED * modifier * TRACK_SPEED);
+                    cameraXform.t.setY(cameraXform.t.getY() + mouseDeltaY * MOUSE_SPEED * modifier * TRACK_SPEED);
+                }
             }
         });
     }
+
+        private void handleKeyboard(Scene scene) {
+            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) {
+                    switch (event.getCode()) {
+                        case Z:
+                            cameraXform.t.setX(0.0);
+                            cameraXform.t.setY(0.0);
+                            camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+                            cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+                            cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+                            break;
+                        case X:
+                            axisGroup.setVisible(!axisGroup.isVisible());
+                            break;
+                        case V:
+                            world.setVisible(!world.isVisible());
+                            break;
+                    }
+                }
+           });
+        };
 
     public static void main(String[] args) {
         launch(args);
