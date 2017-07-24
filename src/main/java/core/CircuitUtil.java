26a20052d1e155e453c4ec5d2e610d5db738e780
Nikolay Frik <nfrik@iphonedreams.com>
2017-07-23 21:28:37 -0400

    Added interface and a class for circuit generator

---
+++ src/main/java/core/CircuitUtil.java
@@ -0,0 +1,18 @@
+package core;
+
+/**
+ * Created by nfrik on 7/23/17.
+ */
+public class CircuitUtil implements CircuitUtilInterface{
+
+    @Override
+    public Graph generateRandomCircuit(String type, int size, int dim) {
+
+        return null;
+    }
+
+    @Override
+    public boolean exportGraphToFile(Graph g, String path) {
+        return false;
+    }
+}
